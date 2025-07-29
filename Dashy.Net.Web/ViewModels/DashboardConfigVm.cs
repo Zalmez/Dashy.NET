798be79b72e27692106d3c8bb5c95c15e8bd4bd6
Cato Kristoffer Myhre <cato.myhre@coop.no>
2025-07-29 23:55:35 +0200

    fix: Fixed an issue when trying to enter edit mode on "/" the user would be rejected because no id was specified

--- Dashy.Net.Web/ViewModels/DashboardConfigVm.cs
+++ Dashy.Net.Web/ViewModels/DashboardConfigVm.cs
@@ -2,6 +2,7 @@
 {
     public class DashboardConfigVm
     {
+        public int Id { get; set; } = 1;
         public string Title { get; set; } = "Dashy.Net";
         public List<SectionVm> Sections { get; set; } = new();
     }
